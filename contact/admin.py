b9e935d62b25ae9de24ed67b21a1f6c4d09678b4
Guilherme Camargo <gui.ferreira.camargo@gmail.com>
2023-10-10 13:29:08 -0300

    Imagens nos models

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,18 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = '-id',
+    list_per_page = 10
+    list_max_show_all = 50
+    list_filter = 'last_name',
+    list_editable = 'phone',
+    list_display_links = 'id', 'first_name',
+    search_fields = 'id', 'first_name', 'last_name',
+
+# Remy Lacroix
+# Gold Starr
+# Marsha May
+# Leah Gotti
+# Gabbie Carter
+# Hazel Moore
--- contact/models.py
+++ contact/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
+# show (boolean), picture (imagem)
 
 # Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
+# category (foreign key), owner (foreign key)
 
 
 class Contact(models.Model):
@@ -17,6 +17,10 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
 
+    # Função que alterar o nome do objeto mostrado no formulário
+    # inserido na base de dados
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
