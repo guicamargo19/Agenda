d0d15186e79d4b5634cb5da122499c1e82fb6369
Guilherme Camargo <gui.ferreira.camargo@gmail.com>
2023-10-11 10:19:29 -0300

    Django Language conf & CSS

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from contact.models import Contact
 
 
 def index(request):
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by("-id")[:10]
+
+    print(contacts.query)
+
+    context = {
+        "contacts": contacts,
+    }
     return render(
         request,
         'contact/index.html',
+        context,
     )
