c244cca58b38c90e1bd02f09cfce0ccabb720031
Guilherme Camargo <gui.ferreira.camargo@gmail.com>
2023-10-12 19:36:06 -0300

    Delete view

--- contact/urls.py
+++ contact/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     # Contact CRUD
     path("contact/<int:contact_id>/detail/", views.contact, name='contact'),
     path("contact/<int:contact_id>/update/", views.update, name='update'),
+    path("contact/<int:contact_id>/delete/", views.delete, name='delete'),
     path("contact/create/", views.create, name='create'),
 ]
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -67,7 +67,7 @@ def update(request, contact_id):
         )
 
     context = {
-        "site_title": 'Create - ',
+        "site_title": 'Update - ',
         'form': ContactForm(instance=contact),
         'form_action': form_action,
     }
@@ -75,5 +75,29 @@ def update(request, contact_id):
     return render(
         request,
         'contact/create.html',
+        context,
+    )
+
+
+# View para deletar um contato
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect("contact:index")
+
+    context = {
+        "site_title": 'Delete - ',
+        'confirmation': confirmation,
+        'contact': contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context,
     )
