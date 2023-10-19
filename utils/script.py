d7aa1f20dbddf85120574efa3149813ff52e4133
camargo <gui.ferreira.camargo@gmail.com>
2023-10-19 17:14:55 -0300

    Update gitignore

--- utils/script.py
+++ utils/script.py
@@ -9,7 +9,7 @@ import django
 from django.conf import settings
 
 DJANGO_BASE_DIR = Path(__file__).parent.parent
-NUMBER_OF_OBJECTS = 1000
+NUMBER_OF_OBJECTS = 1005
 
 sys.path.append(str(DJANGO_BASE_DIR))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
